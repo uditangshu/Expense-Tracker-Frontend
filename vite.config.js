14164436f52c53262a39ce3c941841cf03b6d92f
uditangshu <145389897+uditangshu@users.noreply.github.com>
2024-06-30 23:31:51 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    outDir: 'dist', // Output directory for production builds
+    sourcemap: true, // Enable source maps
+    // You can configure additional build options here
+  },
+});
